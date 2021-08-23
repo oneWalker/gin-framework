2bbcd0d04b23a9b1a37658e194b838c04518fbfa
oneWalker <lewkun@foxmail.com>
2021-08-23 16:39:39 +0800

    feature:完成了mysql的增删改查的demo

--- controller/mysql.go
+++ controller/mysql.go
@@ -1,8 +1,11 @@
 package controller
 
 import (
+	"fmt"
 	model "gin-practice/model/mysql"
 	"gin-practice/model/response"
+	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
@@ -13,7 +16,19 @@ var (
 )
 
 func AddFoo(ctx *gin.Context) {
-
+	var create *model.Foo
+	if err := ctx.BindJSON(&create); err != nil {
+		logrus.Fatalf("parma auth faild %v", err)
+		return
+	}
+	id, err := foo.Insert(create)
+	if err != nil {
+		logrus.Fatalf("parma auth faild %v", err)
+		return
+	}
+	create.ID = uint(id)
+	logrus.Info("%v", id)
+	response.SUCCESS(create, ctx)
 }
 
 func GetFoo(ctx *gin.Context) {
@@ -27,9 +42,25 @@ func GetFoo(ctx *gin.Context) {
 }
 
 func PutFoo(ctx *gin.Context) {
-
+	var put *model.Foo
+	if err := ctx.BindJSON(&put); err != nil {
+		logrus.Fatalf("parma auth faild %v", err)
+		return
+	}
+	err := foo.Update(put)
+	if err != nil {
+		logrus.Fatalf("parma auth faild %v", err)
+		return
+	}
+	response.SUCCESS(true, ctx)
 }
 
 func DeleteFoo(ctx *gin.Context) {
-
+	ids := ctx.Query("ids")
+	id_arr := strings.Split(ids, `,`)
+	for _, v := range id_arr {
+		num, _ := strconv.Atoi(v)
+		foo.Delete(num)
+	}
+	response.SUCCESS(true, ctx)
 }
--- model/mysql/foo.go
+++ model/mysql/foo.go
@@ -10,16 +10,18 @@ import (
 
 type Foo struct {
 	*gorm.Model
-	ID       int64  `gorm:"primary_key:unique"`
+	// 只有注释掉才能得到对应的值
+	// ID       int64  `gorm:"primary_key:unique"`
 	Name     string `gorm:"no null"`
 	Password string `gorm:"no null"`
 }
 
 var Foos []Foo
 
-func (f *Foo) Insert(foo Foo) (id int64, err error) {
+func (f *Foo) Insert(foo *Foo) (id int, err error) {
 	create := mysql.DB.Create(&foo)
-	id = foo.ID
+	fmt.Println(create.Value, foo.ID)
+	id = int(foo.ID)
 	if create.Error != nil {
 		return 0, create.Error
 	}
@@ -38,7 +40,14 @@ func (f *Foo) GetAll() (foos []Foo, err error) {
 	//Struct & Map条件
 	//result := mysql.DB.Where(&Foo{Name: "test2", Password: "test2"}).Find(&foos)
 	result := mysql.DB.Where(map[string]interface{}{"name": "test2", "password": "test2"}).Offset(3).Limit(2).Order("ID desc").Find(&foos)
+
 	fmt.Println(result)
+	//对于结果进行循环
+	for k, v := range foos {
+		v.Name = "name"
+		foos[k] = v
+		fmt.Println("foo", k, v)
+	}
 	//利用主键进行切片
 	//result := mysql.DB.Where([]int{5, 11, 15}).Find(&foos)
 	if result.Error != nil {
@@ -64,23 +73,33 @@ func (f *Foo) GetOne() (foo Foo, err error) {
 	return
 }
 
-func (f *Foo) Update(foo Foo) (id int64, err error) {
+func (f *Foo) Update(foo *Foo) (err error) {
 	//事物和相应的更新函数
+	err = nil
 	tx := mysql.DB.Begin()
+	defer func() {
+		if err := recover(); err != nil {
+			tx.Rollback()
+		}
+	}()
 	//不使用Hooks的话则定义使用UpdateColumn，UpdateColumns
-	tx.Model(&Foo{}).Where("active = ?", true).Update("name", "password")
-	tx.Rollback()
+	tx.Model(&Foo{}).Where("id = ?", 80).Updates(&foo)
+	//tx.Model(&Foo{}).Where("active = ?", true).Where("id = ?", 80).Update("name", "rename")
+	//tx.Rollback()
 	tx.Commit()
-	return 0, nil
+	return err
 }
 
 func (f *Foo) Delete(id int) (err error) {
 	//根据主键删除
 	//result := mysql.DB.Delete(&Foo{},10)
 	//批量删除，没有任何条件的情况下执行批量删除，GORM 不会执行该操作，并返回 ErrMissingWhereClause 错误对此，你必须加一些条件，或者使用原生 SQL，或者启用 AllowGlobalUpdate
-	result := mysql.DB.Where("name LIKE ?", "%test2%").Delete(Foo{})
+	err = mysql.DB.Where("id = ?", id).Delete(&Foo{}).Error
+	//original sql dialect
+	//mysql.DB.Exec("DELETE FROM foos")
+
 	//result := mysql.DB.Delete(Foo{},"name LIKE ?","%test2%")
-	return result.Error
+	return err
 }
 
 //不实现功能，只是作为样例参考，即没有经过测试
